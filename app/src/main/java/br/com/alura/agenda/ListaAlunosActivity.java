ae553ca3fdab4a1d00c860c733434eb40c73d754
Philippe Ehlert <philippe.ehlert@gmail.com>
2016-12-21 16:27:21 -0200

    Atividades da aula 1

--- app/src/main/java/br/com/alura/agenda/ListaAlunosActivity.java
+++ app/src/main/java/br/com/alura/agenda/ListaAlunosActivity.java
@@ -2,6 +2,8 @@ package br.com.alura.agenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
@@ -9,6 +11,12 @@ public class ListaAlunosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
+
+        String[] alunos = {"Daniel", "Ronaldo", "Jefferson", "Felipe"};
+
+        ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
+        listaAlunos.setAdapter(adapter);
     }
 
 }
