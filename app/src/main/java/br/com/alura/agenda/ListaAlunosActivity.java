d6af8d10913e0d0f2feb74cf2af97ba1f67c45d1
Philippe Ehlert <philippe.ehlert@gmail.com>
2016-12-21 15:47:56 -0200

    Criando o projeto no Android Studio

---
+++ app/src/main/java/br/com/alura/agenda/ListaAlunosActivity.java
@@ -0,0 +1,14 @@
+package br.com.alura.agenda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListaAlunosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_alunos);
+    }
+
+}
